f69e75d4db36527b940b5e0e11518212c1cb93f0
Ritesh Sharma <riteshrsharma71@gmail.com>
2024-07-03 22:23:39 +0530

    fibo

--- College/fibonacchiSeries.java
+++ College/fibonacchiSeries.java
@@ -4,11 +4,14 @@ public class fibonacchiSeries {
     public static void main(String[] args){
         Scanner sc =  new Scanner(System.in);
         int n= sc.nextInt();
-        int ans = 0;
+        int ans = 0 ,ans2 = 1;
         System.out.print(ans);
-        System.out.println(ans+1);
+        System.out.print(ans2);
         for(int i = 1 ; i <= n ; i++){
-            
+            int val = ans + ans2;
+            System.out.print(ans+ans2);
+            ans = ans2;
+            ans2 = val;
         }
     }
 }
---
+++ demoClass/first.java
@@ -0,0 +1,7 @@
+public class first {
+    public static void main(String a[]){
+        int num = 1000000000;
+    ;
+        System.out.println(num);
+    }
+}
